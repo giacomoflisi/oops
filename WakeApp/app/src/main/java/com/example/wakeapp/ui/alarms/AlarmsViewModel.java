85cd3be4bb7fc839d05dd87698e09869774898d2
giacomoflisi <giacomoflisi.jack@gmail.com>
2022-03-05 22:14:14 +0100

    added Alarms panel and layout with graphics and text

--- WakeApp/app/src/main/java/com/example/wakeapp/ui/alarms/AlarmsViewModel.java
+++ WakeApp/app/src/main/java/com/example/wakeapp/ui/alarms/AlarmsViewModel.java
@@ -1,16 +1,16 @@
-package com.example.wakeapp.ui.notifications;
+package com.example.wakeapp.ui.alarms;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class AlarmsViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public AlarmsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is home fragment");
     }
 
     public LiveData<String> getText() {
