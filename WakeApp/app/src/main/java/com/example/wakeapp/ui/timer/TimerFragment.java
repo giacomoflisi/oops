fa1f26a261946e567d9b143b0a9b77c57e8c92a2
giacomoflisi <giacomoflisi.jack@gmail.com>
2022-03-18 15:05:12 +0100

    start of timerfragment functionality implementations

--- WakeApp/app/src/main/java/com/example/wakeapp/ui/timer/TimerFragment.java
+++ WakeApp/app/src/main/java/com/example/wakeapp/ui/timer/TimerFragment.java
@@ -2,6 +2,8 @@ package com.example.wakeapp.ui.timer;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +18,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class TimerFragment extends Fragment implements Button.OnClickListener{
 
-    private FloatingActionButton mStartButton, mPauseButton, getmCancelButton;
+    private FloatingActionButton mStartButton, mPauseButton, mStopButton;
     private EditText hoursEditText, minutesEditText, secondsEditText;
 
     int hoursLeft, minutesLeft, secondsLeft, totalSecondsLeft;
@@ -41,6 +43,11 @@ public class TimerFragment extends Fragment implements Button.OnClickListener{
     }
     */
 
+    private void finishTimer(String message) {
+
+    }
+
+
     @Override
     public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,12 +58,76 @@ public class TimerFragment extends Fragment implements Button.OnClickListener{
 
         View view = inflater.inflate(R.layout.fragment_timer, container, false);
 
+        //setting up edit texts
         hoursEditText = view.findViewById(R.id.hours);
         minutesEditText = view.findViewById(R.id.minutes);
         secondsEditText = view.findViewById(R.id.seconds);
 
+        hoursEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() == 2){
+                    minutesEditText.requestFocus();
+                }
+            }
+        });
+
+        minutesEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() == 2){
+                    secondsEditText.requestFocus();
+                }
+            }
+        });
+
+        //setting up buttons
+        mStartButton = view.findViewById(R.id.start_button);
+        mPauseButton = view.findViewById(R.id.pause_button);
+        mStopButton = view.findViewById(R.id.stop_button);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        mPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
         return view;
+    }
+
+    private void updateRemainingTime(long msUntilFinished){
+        totalSecondsLeft = (int) msUntilFinished / 1000;
+        hoursLeft = totalSecondsLeft / 3600;
+        minutesLeft = (totalSecondsLeft % 3600) / 6;
+        secondsLeft = totalSecondsLeft % 60;
+
+        hoursLeftText.setText(String.format("%02d", hoursLeft));
+        minutesLeftText.setText(String.format("%02d", minutesLeft));
+        secondsLeftText.setText(String.format("%02d", secondsLeft));
+
     }
 
     @Override
